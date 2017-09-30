1eb0b2f0b19f5d54e8d41ba8fd64abcb20458e7e
= <mars2022@vip.qq.com>
2017-09-30 15:27:37 +0800

    在每次刷新结束后移除subview

--- CellsGame/ViewController.m
+++ CellsGame/ViewController.m
@@ -12,11 +12,6 @@
 
 @end
 
-//struct Cell{
-//    int cellID;
-//    int cellState;
-//};
-
 @implementation ViewController
 int oldCells[9][9];
 int newCells[9][9];
@@ -31,7 +26,6 @@ int newCells[9][9];
     }
     // 绘制细胞图层
     [self drawCells];
-    
 }
 
 //刷新细胞
@@ -42,27 +36,27 @@ int newCells[9][9];
             int tempX = i-1;
             int tempY = j-1;
             for(int y = tempY; y<9 && y<tempY+3; y++)
-//                if (tempX >= 0 && y>=0 && *(oldcell + tempX * 9 + y) == 1) {
+                //                if (tempX >= 0 && y>=0 && *(oldcell + tempX * 9 + y) == 1) {
                 if(tempX >= 0 && y >= 0 && oldCells[tempX][y] == 1){
                     count ++;
                 }
             for(int y = tempY; y<9 && y<tempY+3; y++){
-//                if (y != j && *(oldcell +(tempX+1)*9 +y) == 1 && y >= 0){
+                //                if (y != j && *(oldcell +(tempX+1)*9 +y) == 1 && y >= 0){
                 if(y != j && oldCells[tempX+1][y] == 1 && y >= 0){
                     count ++;
                 }
             }
             for (int y = tempY; y < 9 && y < tempY + 3; y++) {
-//                if (*(oldcell +(tempX + 2)*9 +y) == 1 && y >= 0) {
+                //                if (*(oldcell +(tempX + 2)*9 +y) == 1 && y >= 0) {
                 if(oldCells[tempX+2][y] == 1 && y >= 0){
                     count ++;
                 }
             }
             if (count == 3) {
-//                *(newcell + i * 9 + j) = 1;
+                //                *(newcell + i * 9 + j) = 1;
                 newCells[i][j] = 1;
             }else if(count != 2){
-//                *(newcell + i * 9 + j) = 0;
+                //                *(newcell + i * 9 + j) = 0;
                 newCells[i][j] = 0;
             }
             count = 0;
@@ -101,20 +95,24 @@ int newCells[9][9];
 }
 
 
+- (void)removeAllSubviews {
+    while (_backgroundView.subviews.count) {
+        UIView* child = _backgroundView.subviews.lastObject;
+        [child removeFromSuperview];
+    }
+}
+
 - (IBAction)startButton:(id)sender {
     //刷新100次
     for (int i = 0; i< 100; i++){
-        if(i%2 == 0){
-            [self refresh:oldCells to:newCells];
-            [self drawCells];
-        }else{
-            [self refresh:newCells to:oldCells];
-            [self drawCells];
-        }
+        [self removeAllSubviews];
+        [self refresh:oldCells to:newCells];
+        [self drawCells];
     }
 }
 
 - (IBAction)stopButton:(id)sender {
+    [self removeAllSubviews];
     // 初始化细胞
     for(int i = 0;i<9;i++){
         for (int j = 0; j<9; j++) {
@@ -125,4 +123,6 @@ int newCells[9][9];
     // 绘制细胞图层
     [self drawCells];
 }
+
+
 @end
